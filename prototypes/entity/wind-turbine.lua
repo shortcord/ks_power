63cea4981066f2de802c605a26fbead5f2dd6685
Tristan Smith <tristan@shortcord.com>
2016-04-23 13:11:51 -0400

    refuckering

--- prototypes/entity/wind-turbine.lua
+++ prototypes/entity/wind-turbine.lua
@@ -10,20 +10,7 @@ data:extend({
 		stack_size = 30,
 	},
 
-	{
-    type = "recipe",
-    name = "wind-turbine",
-    energy_required = 4,
-    enabled = "true",
-    ingredients =
-    {
-      {"iron-plate", 8},
-      {"iron-gear-wheel", 4},
-	  {"copper-cable", 8},
-	  {"iron-stick", 5}
-    },
-    result = "wind-turbine"
-  },
+	
 
   {
 		type = "generator",
